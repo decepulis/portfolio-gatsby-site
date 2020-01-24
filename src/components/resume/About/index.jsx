566cfc429f1d54472724daa8fce412774c2cd910
Darius Cepulis <dcepulis@microsensorlabs.com>
2020-01-24 14:57:53 -0600

    A cooler wave

--- src/components/resume/About/index.jsx
+++ src/components/resume/About/index.jsx
@@ -11,7 +11,7 @@ import {
   StyledLogos,
 } from "./styles"
 
-import headshot from "./headshot-web.jpg"
+import headshot from "./headshot-bw-web.jpg"
 import microsensor from "./logo-microsensor.svg"
 import bosch from "./logo-bosch.svg"
 import bmw from "./logo-bmw.svg"
@@ -59,6 +59,46 @@ export default function About() {
         </StyledCompanies>
         <StyledWave
           xmlns="http://www.w3.org/2000/svg"
+          preserveAspectRatio="none"
+          viewBox="0 0 1920 170"
+        >
+          <defs>
+            <linearGradient
+              x1="49.253%"
+              y1="85.804%"
+              x2="49.253%"
+              y2="43.074%"
+              id="a"
+            >
+              <stop stop-color="#FFF" offset="0%"></stop>
+              <stop stop-color="#FFF" offset="100%"></stop>
+            </linearGradient>
+          </defs>
+          <g fill="none">
+            <path
+              d="M1920 4.719v69.5c-362.63 60.036-692.797 55.536-990.5-13.5C565.833-23.615 256 12.813 0 170L1 4.719h1919z"
+              fill="url(#a)"
+              transform="rotate(180 960.5 87.36)"
+            ></path>
+            <path
+              d="M1 170V99c269.033-70.44 603.533-66.44 1003.5 12C1494 207 1921 4.719 1921 4.719L1920 170H1z"
+              fill-opacity=".3"
+              fill="#FFF"
+            ></path>
+            <path
+              d="M1 170.75V99C373.115 4.216 705.281-4.951 997.5 71.5c365.667 95.667 673.5 73.406 923.5-66.781l-1 166.031H1z"
+              fill-opacity=".3"
+              fill="#FFF"
+            ></path>
+            <path
+              d="M1 170v-67C400.333-1.333 744.167-19 1032.5 50c432.5 103.5 754 19.219 888.5-45.281l-1 166.031L1 170z"
+              fill-opacity=".35"
+              fill="#FFF"
+            ></path>
+          </g>
+        </StyledWave>
+        {/* <StyledWave
+          xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 1440 126"
           aria-hidden="true"
           hidden=""
@@ -67,7 +107,7 @@ export default function About() {
             fill="#fff"
             d="M1440,21.2101911 L1440,120 L0,120 L0,21.2101911 C120,35.0700637 240,42 360,42 C480,42 600,35.0700637 720,21.2101911 C808.32779,12.416393 874.573633,6.87702029 918.737528,4.59207306 C972.491685,1.8109458 1026.24584,0.420382166 1080,0.420382166 C1200,0.420382166 1320,7.35031847 1440,21.2101911 Z"
           ></path>
-        </StyledWave>
+        </StyledWave> */}
       </StyledSection>
     </>
   )
