947759842b8d4d23b33825ebcc98d6a79a2e2ba4
Darius Cepulis <dcepulis@microsensorlabs.com>
2020-01-27 16:02:26 -0600

    Remove some opld crud from gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,4 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl:
-      process.env.NODE_ENV === "production"
-        ? "https://decepulis.netlify.com"
-        : "http://localhost:8000",
-  },
   plugins: [
     `gatsby-plugin-netlify-cms`,
     `gatsby-transformer-remark`,
