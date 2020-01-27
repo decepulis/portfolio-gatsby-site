2b32b5dd56335101be99dd847d55caae42402ee0
Darius Cepulis <dcepulis@microsensorlabs.com>
2020-01-27 16:02:20 -0600

    UseEffect patch for loading themes from localstorage

--- src/components/contexts/ThemeIdContext.jsx
+++ src/components/contexts/ThemeIdContext.jsx
@@ -1,5 +1,6 @@
 import React, {
   useState,
+  useEffect,
   useCallback,
   useMemo,
   useContext,
@@ -19,37 +20,50 @@ const themeOptions = Object.entries(themes).map(([themeKey, themeValue]) => [
   themeValue.label,
 ])
 
-// const initThemeId =
-//   typeof window !== "undefined"
-//     ? localStorage.getItem("themeId") ?? "mono"
-//     : "1989"
-
-// setting this as constant until I figure out what's going on with hydration
-const initThemeId = "1989"
+const initThemeId =
+  typeof window !== "undefined"
+    ? // theme should be retrieved from localstorage or default to 1989
+      localStorage.getItem("themeId") ?? "1989"
+    : // ssr should generate 1989 pages
+      "1989"
 
 export const ThemeIdContext = createContext()
 
 export const ThemeIdProvider = ({ children }) => {
-  const [themeId, setThemeId] = useState(initThemeId)
+  const [themeId, setThemeId] = useState()
 
+  // Keep local state and localstorage in sync
   const setAndStoreThemeId = useCallback(
     newThemeId => {
-      // localStorage.setItem("themeId", newThemeId)
+      localStorage.setItem("themeId", newThemeId)
       setThemeId(newThemeId)
     },
     [setThemeId]
   )
+  // set themeId after load to work around
+  // https://github.com/styled-components/styled-components/issues/2629
+  useEffect(() => {
+    setAndStoreThemeId(initThemeId)
+  }, [setThemeId])
 
+  // memoize the provider values.
+  // just for fun.
   const themeIdProviderValue = useMemo(
     () => [themeId, setAndStoreThemeId, themeOptions],
     [themeId, setAndStoreThemeId]
   )
+  const themeProviderValue = useMemo(
+    () => ({
+      id: themeId,
+      ...themes[themeId],
+    }),
+    [themeId]
+  )
 
+  // Wrap the page in the providers!
   return (
     <ThemeIdContext.Provider value={themeIdProviderValue}>
-      <ThemeProvider theme={{ id: themeId, ...themes[themeId] }}>
-        {children}
-      </ThemeProvider>
+      <ThemeProvider theme={themeProviderValue}>{children}</ThemeProvider>
     </ThemeIdContext.Provider>
   )
 }
